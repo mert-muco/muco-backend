e5f70917b3c5408c893b356b59d71eb21b3d3f17
tengiz <tengizumut@gmail.com>
2025-11-03 19:18:31 +0000

    settings allow

--- muco_django/muco_django/settings.py
+++ muco_django/muco_django/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://muco-backend-production.up.railway.app/']
+ALLOWED_HOSTS = ['muco-backend-production.up.railway.app']
 
 
 # Application definition
